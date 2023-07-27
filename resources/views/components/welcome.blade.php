82c74059be4c155c11c380114f019f5ddc7de9de
Amol Bansode <aamolbansode@gmail.com>
2023-07-27 05:44:21 +0530

    added dummy box for dashboard

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,8 +1,17 @@
-<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <h1 class="mt-8 text-2xl text-center font-medium text-gray-900">
-        Welcome to your Techclinic Lead Tracker!!
-    </h1>
-</div>
-
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8"> 
+<div class="bg-white p-6 rounded-lg shadow-lg">
+    <h2 class="text-gray-800 text-3xl font-bold mb-6">Welcome to your Techclinic Lead Tracker!!</h2>
+    <div class="grid grid-cols-3 gap-4">
+        <div class="bg-gray-100 p-4 rounded-lg shadow-md text-center">
+            <span class="text-gray-700 text-lg font-semibold mb-2">Total Active Leads</span>
+            <p class="text-blue-600 text-3xl font-bold">100</p>
+        </div>
+        <div class="bg-gray-100 p-4 rounded-lg shadow-md text-center">
+            <span class="text-gray-700 text-lg font-semibold mb-2">Converted Leads</span>
+            <p class="text-green-600 text-3xl font-bold">20</p>
+        </div>
+        <div class="bg-gray-100 p-4 rounded-lg shadow-md text-center">
+            <span class="text-gray-700 text-lg font-semibold mb-2">Not Converted Leads</span>
+            <p class="text-red-600 text-3xl font-bold">80</p>
+        </div>
+    </div>
 </div>
